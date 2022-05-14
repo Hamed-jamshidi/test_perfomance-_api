1d9ec1c966b3ff51d9de76d679c0018de614907c
Hamed-jamshidi <jamshidi7011@gmail.com>
2022-05-14 16:41:49 +0430

    after change and test with pgadmin

--- api_test_parallel.py
+++ api_test_parallel.py
@@ -5,11 +5,19 @@ import time
 import json
 import random
 URL = 'http://localhost:3008/api/authAdmin/login'
+from main import register
 
+with open('test_user.txt') as f:
+    lines = f.readlines()
+print("Done")
 def fetch(session, url):
-    data = {"username": "Justin60", "password": 7300}
+    global lines
+    data = lines[random.randint(0, len(lines))].strip().replace("'", "\"")
+    data = json.loads(data)
     with session.post(url, data) as response:
         print(response.json())
+
+
 
 
 time1 = time.time()
--- main.py
+++ main.py
@@ -6,26 +6,29 @@ import time
 fake = Faker('en_US')
 url = "http://localhost:3008/api/authAdmin/register"
 time1 = time.time()
-for _ in range(10):
-    my_dic = {"firstname" : fake.first_name(),
-              "lastname": fake.last_name(),
-              "username": fake.first_name()+str(randint(10, 99)),
-              "email": fake.email(),
-              "password": randint(1000, 9999),
-              "isadmin": False
-              }
-    print(my_dic)
-    response = requests.post(url, my_dic)
-    print(response.json())
-    print(response.status_code)
-    if(response.status_code == 200):
+def register():
+    for _ in range(100):
+        my_dic = {"firstname": fake.first_name(),
+                  "lastname": fake.last_name(),
+                  "username":fake.first_name() + str(randint(100000, 990000)),
+                  "email": fake.email(),
+                  "password": randint(1000, 9999),
+                  "isadmin": 0
+                  }
+        print(my_dic)
+        response = requests.post(url, my_dic)
         print(response.json())
-        with open('test_user.txt', "a") as f:
-            f.writelines(str(my_dic))
-            f.write('\n')
-            f.close()
+        print(response.status_code)
+        if (response.status_code == 200):
+            print(response.json())
+            with open('test_user.txt', "a") as f:
+                f.writelines(str(my_dic))
+                f.write('\n')
+                f.close()
 
+#register()
 time2 = time.time()
+
 delta = time2 - time1
 print(delta)
 
